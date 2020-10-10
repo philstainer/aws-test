1d433b9dc7a39572f8737fbb2c5e5f76ce0aa5ad
Phil Stainer <philstainer.dev@gmail.com>
2020-10-10 16:18:30 +0100

    Test

--- backend/index.mjs
+++ backend/index.mjs
@@ -2,6 +2,6 @@ import express from 'express'
 
 const app = express()
 
-app.route('/').get((req, res) => res.json({message: 'OK'}))
+app.route('/').get((req, res) => res.json({message: 'Ian'}))
 
 app.listen(4000)
